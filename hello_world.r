62b48c4ae83ac93bab41f44ffaad5971ddeba2f5
narupong.m08@gmail.com <narupong.m08@gmail.com>
2023-08-05 14:31:29 +0000

    Add second plot

--- hello_world.r
+++ hello_world.r
@@ -8,12 +8,6 @@ mtcars %>%
   select(1:5) %>%
   filter(mpg > 30)
 
-# first plot
-mtcars %>%
-  select(mpg) %>%
-  filter(mpg < 30)
-
-# second plot
 mtcars %>%
   select(mpg) %>%
   filter(mpg < 30)
